910a524d86df7f2583b8ee310cc4040ed51a6eb8
NayanPal01 <167202703+NayanPal01@users.noreply.github.com>
2025-12-16 01:11:26 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.04%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int solve(int a,int b, int n){
+        if(n==0) return a;
+        return solve(b,a+b,n-1);
+            
+        solve(b,a+b,n-1);
+    }
+    int fib(int n) {
+        return solve(0,1,n);
+    }
+};
